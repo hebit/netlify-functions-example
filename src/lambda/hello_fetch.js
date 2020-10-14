4499e69fd51a16321a987a5acd9919c582205be2
Hebert Oliveira <hebertoliveira@infojr.com.br>
2020-10-13 22:44:09 -0300

    change fetch example to test with jsonplaceholder API

--- src/lambda/hello_fetch.js
+++ src/lambda/hello_fetch.js
@@ -1,13 +1,13 @@
 import fetch from "node-fetch";
 
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
+const API_ENDPOINT = "https://jsonplaceholder.typicode.com/todos/1";
 
 exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
-    .then(response => response.json())
-    .then(data => ({
+  return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
+    .then((response) => response.json())
+    .then((data) => ({
       statusCode: 200,
-      body: data.joke
+      body: data,
     }))
-    .catch(error => ({ statusCode: 422, body: String(error) }));
+    .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
