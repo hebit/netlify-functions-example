8f442d1a8a85adbff1054fc9155c8a04771b63e6
Hebert Oliveira <hebertoliveira@infojr.com.br>
2020-10-13 22:48:37 -0300

    string response on body lambda function

--- src/lambda/hello_fetch.js
+++ src/lambda/hello_fetch.js
@@ -1,13 +1,13 @@
 import fetch from "node-fetch";
 
-const API_ENDPOINT = "https://jsonplaceholder.typicode.com/todos/1";
+const API_ENDPOINT = "https://jsonplaceholder.typicode.com/posts";
 
 exports.handler = async (event, context) => {
   return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
     .then((response) => response.json())
     .then((data) => ({
       statusCode: 200,
-      body: data,
+      body: JSON.stringify(data),
     }))
     .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
